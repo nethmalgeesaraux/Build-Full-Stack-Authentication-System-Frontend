59946ed86d2a5519d0893b1a80a18162b5b70f7c
nethmalgeesaraux <nethmalpaypal@gmail.com>
2026-04-14 22:55:36 +0530

    integrate routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import EmailVerify from './pages/EmailVerify';
+import ResetPassword from './pages/RestPassword';
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <ToastContainer />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/email-verify" element={<EmailVerify />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
+      </Routes>
+
+    </div>
   )
 }
 
