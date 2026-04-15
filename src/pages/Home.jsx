df3222d715067634d751fa0f748599ff6ff863c3
nethmalgeesaraux <nethmalpaypal@gmail.com>
2026-04-15 22:02:58 +0530

    home upadate

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import Menubar from '../components/Menubar'
+import Header from '../components/Header'
 
 const Home = () => {
   return (
-   <Menubar/>
+    <div className="flex flex-col items-center justify-content-center min-vh-100">
+      <Menubar />
+      <Header/>
+    </div>
+
   )
 }
 
