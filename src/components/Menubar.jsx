153c1cb37af965f5b500448573cb9eb29b119fe2
nethmalgeesaraux <nethmalpaypal@gmail.com>
2026-04-15 21:36:53 +0530

    navigate login

--- src/components/Menubar.jsx
+++ src/components/Menubar.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import logoHome from '../assets/Logo_home.png';
+import { useNavigate } from 'react-router-dom';
 
 const Menubar = () => {
+
+    const navigate = useNavigate();
+
     return (
         <nav className="navbar bg-white px-5 py-4 d-flex justify-content-between align-items-center">
             <div className="d-flex align-items-center gap-2">
@@ -9,7 +13,7 @@ const Menubar = () => {
                 <span className="fw-bold fs-4 text-dark">Authify</span>
             </div>
 
-            <div className="btn btn-outline-dark rounded-pill px-3">
+            <div className="btn btn-outline-dark rounded-pill px-3" onClick={()=> navigate("/Login")}>
                 Login <i className="bi bi-arrow-right ms-2"></i>
             </div>
         </nav>
